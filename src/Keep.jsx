f139fb6445808b31ef36529ff9a6263995bc9132
Kamal-Acharya <kacharya443@gmail.com>
2022-07-01 20:07:12 +0530

    commit message

--- src/Keep.jsx
+++ src/Keep.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Footer from './Footer';
 import Note from './Note';
 import CreateNote from './CreateNote';
 import './index.css';
 import logo from './1.png';
 
+//const get Item
+const getItemData=()=>{
+   let list= localStorage.getItem('lists');
 
+   if(list)
+   {
+    return JSON.parse(localStorage.getItem('lists'));
+   }
+   else
+   {
+    return [];
+   }
+}
 const Keep=()=>{
-    const [addItem,setAddItem]=useState([]);
+    const [addItem,setAddItem]=useState(getItemData);
 
     const add=(note)=>{
       
@@ -29,6 +41,11 @@ const Keep=()=>{
          }))
 
       };
+
+      //local storage
+      useEffect(()=>{
+       localStorage.setItem('lists',JSON.stringify(addItem));
+      },[addItem])
     return(
         <>
         <div className="nav">
